655fd4787f9feda6215496090f27ea66b1d9119f
Adam Kecskes <adam@kecskes.net>
2023-10-23 15:38:05 -0500

    successfully pull word from free dictionary api

--- src/components/GrabAWord.tsx
+++ src/components/GrabAWord.tsx
@@ -3,19 +3,31 @@ import { Word } from '@/data/word'
 import { ZodType, z } from 'zod'
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver as zr } from "@hookform/resolvers/zod";
+import { useState } from 'react';
 
 type JustWord = Partial<Word>
 
 export const GrabAWord = () => {
+  const [word, setWord] = useState<Word>();
+  const [error, setError] = useState<{title: string, message: string, resolution: string}>();
 
   const schema: ZodType<JustWord> = z.object({
     word: z.string().min(2).max(30)
   })
 
-  const { register, handleSubmit, formState: { errors } } = useForm<JustWord>({ resolver: zr(schema) })
+  const { register, handleSubmit, formState: { errors: formErrors } } = useForm<JustWord>({ resolver: zr(schema) })
 
-  const handleValidatedData: SubmitHandler<JustWord> = (data) => {
-    console.log(`got the word «${data.word}»`)
+  const handleValidatedData: SubmitHandler<JustWord> = async (data) => {
+      const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${data.word}`)
+      if (!res.ok) {
+        setWord(undefined)
+        setError(JSON.parse(await res.text()))
+        return
+      }
+      
+      const words = await res.json()
+      console.info(words[0])
+      setWord(words[0])
   }
 
   return (
@@ -24,9 +36,10 @@ export const GrabAWord = () => {
       <form className='flex flex-col' onSubmit={handleSubmit(handleValidatedData)}>
         <label>What word should I grab?</label>
         <input type="text" {...register("word")} />
-        {errors.word && <span className='text-red-900'>{errors.word.message}</span>}
-        <input type="submit" className='btn' />
+        {formErrors.word && <span className='text-red-900'>{formErrors.word.message}</span>}
+        <input type="submit" className='btn btn-primary' />
       </form>
+      {word ? <>Got the word: {word.word}. There are {word.meanings.length} meanings.</>  : error && error.message } 
     </div>
   )
 }
