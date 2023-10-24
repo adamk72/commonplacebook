8e4dd251d5d41596e3bbb27e8d60502bf067b4ed
Adam Kecskes <adam@kecskes.net>
2023-10-23 20:07:24 -0500

    install ky as alt to fetch; if not this, then try

--- src/components/GrabAWord.tsx
+++ src/components/GrabAWord.tsx
@@ -4,12 +4,13 @@ import { ZodType, z } from 'zod'
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver as zr } from "@hookform/resolvers/zod";
 import { useState } from 'react';
+import ky, {HTTPError} from 'ky'
 
 type JustWord = Partial<Word>
 
 export const GrabAWord = () => {
   const [word, setWord] = useState<Word>();
-  const [error, setError] = useState<{title: string, message: string, resolution: string}>();
+  const [error, setError] = useState<{ title: string, message: string, resolution: string }>();
 
   const schema: ZodType<JustWord> = z.object({
     word: z.string().min(2).max(30)
@@ -18,16 +19,16 @@ export const GrabAWord = () => {
   const { register, handleSubmit, formState: { errors: formErrors } } = useForm<JustWord>({ resolver: zr(schema) })
 
   const handleValidatedData: SubmitHandler<JustWord> = async (data) => {
-      const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${data.word}`)
-      if (!res.ok) {
-        setWord(undefined)
-        setError(JSON.parse(await res.text()))
-        return
-      }
-      
-      const words = await res.json()
-      console.info(words[0])
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${data.word}`
+    try {
+      const words: Word[] = await ky.get(url).json()
       setWord(words[0])
+    } catch (error) {
+      setWord(undefined)
+      if (error instanceof HTTPError && error.name === 'HTTPError') {
+        setError(await error.response.json())
+      }
+    }
   }
 
   return (
@@ -39,7 +40,7 @@ export const GrabAWord = () => {
         {formErrors.word && <span className='text-red-900'>{formErrors.word.message}</span>}
         <input type="submit" className='btn btn-primary' />
       </form>
-      {word ? <>Got the word: {word.word}. There are {word.meanings.length} meanings.</>  : error && error.message } 
+      {word ? <>Got the word: {word.word}. There are {word.meanings.length} meanings.</> : error && error.message}
     </div>
   )
 }
