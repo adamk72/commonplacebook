c692155038e584b85300147bb1cbfe5fdfeec853
Adam Kecskes <adam@kecskes.net>
2023-10-24 16:25:46 -0500

    refactor SignUp to be lower profile

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { FormEvent, useRef } from "react"
+import { FormEvent, useRef, useState } from "react"
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ZodType, z } from 'zod';
@@ -7,6 +7,7 @@ import { ZodType, z } from 'zod';
 import Button from "../Button"
 import LabelAndField from "./LabelAndField"
 import LabelLink from "./LabelLink"
+import FormSurface from '../FormSurface';
 
 export type SignUpIn = {
   email: string,
@@ -20,65 +21,54 @@ const schema: ZodType<SignUpIn> = z.object({
 }).refine((data) => data.password === data.confirm, { message: "Passwords do not match", path: ["confirm"] })
 
 export const SignUp = () => {
+  const [signIn, setSignIn] = useState(false);
   const { register, handleSubmit, formState: { errors: fErrors } } = useForm<SignUpIn>({ resolver: zodResolver(schema) });
 
   const handleValidatedInput: SubmitHandler<SignUpIn> = async (data) => {
   }
 
   return (
-    <div className="flex items-center bg-white dark:bg-gray-900">
-      <div className="container mx-auto">
-        <div className="max-w-md mx-auto my-10">
-          <div className="text-center">
-            <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
-              Sign in
-            </h1>
-            <p className="text-gray-500 dark:text-gray-400">
-              Sign in to access your account
-            </p>
-          </div>
-          <div className="m-7">
-            <form onSubmit={handleSubmit(handleValidatedInput)}>
-              <LabelAndField
-                errors={fErrors}
-                label="Email Address"
-                name="email"
-                placeholder="you@company.com"
-                register={register}
-                type="email"
-              />
-              <LabelAndField
-                errors={fErrors}
-                label="Password"
-                labelLink={<LabelLink text="Forgot password?" href="#!" />}
-                name="password"
-                placeholder="Your Password"
-                register={register}
-                type="password"
-              />
-              <LabelAndField
-                errors={fErrors}
-                label="Confirm Password"
-                name="confirm"
-                placeholder="Your Password"
-                register={register}
-                type="password"
-              />
-              <Button type="submit" label="Sign in" />
-              <p className="text-sm text-center text-gray-400">
-                Don&#x27;t have an account yet?{" "}
-                <a
-                  href="#!"
-                  className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800"
-                >
-                  Sign up
-                </a>
-                .
-              </p>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    <FormSurface title="Sign in" subtitle="Sign in to access your account">
+      <>
+        <form onSubmit={handleSubmit(handleValidatedInput)}>
+          <LabelAndField
+            errors={fErrors}
+            label="Email Address"
+            name="email"
+            placeholder="you@company.com"
+            register={register}
+            type="email"
+          />
+          <LabelAndField
+            errors={fErrors}
+            label="Password"
+            labelLink={<LabelLink text="Forgot password?" href="#!" />}
+            name="password"
+            placeholder="Your Password"
+            register={register}
+            type="password"
+          />
+          <LabelAndField
+            errors={fErrors}
+            label="Confirm Password"
+            name="confirm"
+            placeholder="Your Password"
+            register={register}
+            type="password"
+          />
+          <Button type="submit" label="Sign in" />
+        </form>
+        <p className="text-sm text-center text-gray-400">
+          Don&#x27;t have an account yet?{" "}
+          <a
+            href="#!"
+            className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800"
+          >
+            Sign up
+          </a>
+          .
+        </p>
+      </>
+    </FormSurface >
   )
 }
---
+++ src/components/FormSurface.tsx
@@ -0,0 +1,25 @@
+import { ReactElement } from 'react'
+
+const FormSurface = ({ title, subtitle, children }: { title: string, subtitle: string, children: ReactElement }) => {
+  return (
+    <div className="flex items-center bg-white dark:bg-gray-900">
+      <div className="container mx-auto">
+        <div className="max-w-md mx-auto my-10">
+          <div className="text-center">
+            <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+              {title}
+            </h1>
+            <p className="text-gray-500 dark:text-gray-400">
+              {subtitle}
+            </p>
+          </div>
+          <div className="m-7">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FormSurface
