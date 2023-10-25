7fe0a7a936cc0b34123aa44ccac0e91abca0565b
Adam Kecskes <adam@kecskes.net>
2023-10-25 14:39:16 -0500

    base prod on local so only need to change in one place

--- src/lib/config/envs/local.ts
+++ src/lib/config/envs/local.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "../defineConfig"
+
+export const defaultConfig = {
+  apiURL: "http://localhost:1337",
+  strapi: { 
+    passwordMinLength: 6 
+  }
+}
+
 export function createLocalConfig() {
-  return defineConfig({
-    apiURL: "http://localhost:1337",
-  })
+  return defineConfig(defaultConfig)
 }
--- src/lib/config/envs/prod.ts
+++ src/lib/config/envs/prod.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "../defineConfig"
+import { defaultConfig } from "./local";
 export function createProdConfig() {
   return defineConfig({
-    apiURL: "https://api.mydomain.com", // TBD
+    ...defaultConfig, apiURL: "https://api.mydomain.com", // TBD
   })
 }
